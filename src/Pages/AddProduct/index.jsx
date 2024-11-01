5ab2fcfa3de3bc6c348eee9d7b61efda09474f3b
QuocViet <vietnguyen142002@gmail.com>
2024-11-01 15:16:06 +0700

    update navigate to products when add product success

--- src/Pages/AddProduct/index.jsx
+++ src/Pages/AddProduct/index.jsx
@@ -1,15 +1,20 @@
 import AddProductForm from '~/Components/Product/AddProductForm';
 import { useMutationAndToast } from '~/Hooks';
-import { QUERY_KEYS } from '~/Constants';
+import { QUERY_KEYS, ROUTES } from '~/Constants';
 import { createProduct } from '~/services';
 import HeaderPage from '~/Components/HeaderPage';
+import { useNavigate } from 'react-router-dom';
 
 function AddProductPage() {
+    const navigate = useNavigate();
     const mutation = useMutationAndToast({
         keys: [QUERY_KEYS.PRODUCTS],
         fn: createProduct,
         loadingString: 'Adding product...',
         successString: 'Adding product success',
+        onSuccess: () => {
+            navigate(ROUTES.PRODUCTS);
+        },
     });
 
     const handleCreateProduct = (values) => {
